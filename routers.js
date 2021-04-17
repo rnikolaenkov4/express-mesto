ec66671601bf2ab7c9e7f8ea2b5aac0d6e44060e
Roman Nikolaenkov <rnikolaenkov@gmail.com>
2021-04-17 17:07:09 +0300

    добавление, чтение и получение пользователя из БД

--- routers.js
+++ routers.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
+const User = require('./models/user');
 
 router.get('/users', (req, res) => {
-  res.send('Get user');
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
 router.get('/users/:id', (req, res) => {
-  res.send(req.params.id);
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
 router.post('/users', (req, res) => {
   const { name, about, avatar } = req.body;
-  res.send(`Name: ${name} | about: ${about} | avatar: ${avatar}`);
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
 module.exports = router;

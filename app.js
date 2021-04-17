fdceca0338c1e4278da5d132e8beca0e94eeb0a0
Roman Nikolaenkov <rnikolaenkov@gmail.com>
2021-04-17 17:17:31 +0300

    длобавил папку routers и перенес туда router для user

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routers');
+const user = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const DB_URL = 'mongodb://localhost:27017/mestodb';
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/users', router);
+app.use('/users', user);
 
 mongoose.connect(DB_URL, {
   useNowUrlParser: true,
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,23 @@
-const router = require('express').Router();
-const User = require('./models/user');
+const users = require('express').Router();
+const User = require('../models/user');
 
-router.get('/', (req, res) => {
+users.get('/', (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
-router.get('/:id', (req, res) => {
+users.get('/:id', (req, res) => {
   User.findById(req.params.id)
     .then((user) => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
-router.post('/', (req, res) => {
+users.post('/', (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
-module.exports = router;
+module.exports = users;

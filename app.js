f0099e1f66ff9db03277295b622517f11e1c24e8
Roman Nikolaenkov <rnikolaenkov@gmail.com>
2021-04-17 20:28:04 +0300

    имитация авторизации

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '607ae7ef2a3ff04180a83032',
+  };
+
+  next();
+});
+
 app.use('/users', user);
 
 mongoose.connect(DB_URL, {
